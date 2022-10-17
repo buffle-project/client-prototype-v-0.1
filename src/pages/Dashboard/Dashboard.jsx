03a8702f52397e0eb50eebdf3ce42469315fc711
Madjarx <88193583+Madjarx@users.noreply.github.com>
2022-10-17 18:23:34 +0200

    temp logout feature on dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useContext } from "react";
+
+import { Context } from "../../context/Context";
+
+import {Button} from 'antd';
 
 function Dashboard() {
+  const { dispatch } = useContext(Context);
+
+  const handleLogout = () => {
+    dispatch({type: "LOGOUT"})
+  }
   return (
-    <div>Dashboard</div>
-  )
+    <>
+      <div>Dashboard says hi!</div>
+      <Button
+        onClick={handleLogout}
+      >Logout</Button>
+    </>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
